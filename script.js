ca209c6dc8d6ddcc23aac457b3ea26636818051d
nardo88 <nardo1988@mail.ru>
2020-11-26 14:12:39 +0300

    исправление усложненного задания

--- script.js
+++ script.js
@@ -10,51 +10,57 @@ let isNumber = function (n) {
     return !isNaN(parseFloat(n)) && isFinite(n)
 }
 
+
 function getNumber(num) {
+
     // создаем счетчик
-    let count = 10
+    let count = 3
+
+    
+   
+    
     return startGame = () => {
+
+        // создаем функцию проверки счетчика
+        function checkCount() {
+            if (confirm('Попытки закончились, хотите попробовать еще раз?')) {
+                count = 3
+                startGame()
+            } else {
+                // если нажали отмену
+                alert('Игра окончена')
+            }
+        }
         // задаем вопрос
         numFromUser = prompt('Введи число!')
-        if (!numFromUser) {
+        if (numFromUser === null) {
             alert('Игра окончена')
         } else if (!isNumber(numFromUser)) {
             startGame()
-        } else if (numFromUser > num) {
+
+        // если введенное число больше загаданного
+        } else if (+numFromUser > num) {
             count--
             if (count === 0) {
-                if (confirm('Попытки закончились, хотите попробовать еще раз?')) {
-                    count = 10
-                    startGame()
-            } {
-                return
-            }
-
-        } else {
-            alert('Загаданное число меньше у вас осталось ' + count + ' попыток')
-            startGame()
-        }
-
-
-    } else if (numFromUser < num) {
-        count--
-        if (count === 0) {
-            if (confirm('Попытки закончились, хотите попробовать еще раз?')) {
-                count = 10
+                checkCount()
+            } else {
+                alert('Загаданное число меньше у вас осталось ' + count + ' попыток')
                 startGame()
-            } {
-                return
             }
-
-        } else {
-            alert('Загаданное число больше у вас осталось ' + count + ' попыток')
-            startGame()
+        // если введенное число меньше загаданного
+        } else if (+numFromUser < num) {
+            count--
+            if (count === 0) {
+                checkCount()
+            } else {
+                alert('Загаданное число больше у вас осталось ' + count + ' попыток')
+                startGame()
+            }
+        // если число равно загаданному
+        } else if (+numFromUser === num) {
+            alert('Поздравляю, Вы угадали!!!')
         }
-
-    } else if (numFromUser == num) {
-        alert('Поздравляю, Вы угадали!!!')
     }
-}
 
 }
 
