474e0126516325a96221e05a24dd3d201dcacfa2
nardo88 <nardo1988@mail.ru>
2020-11-26 12:43:04 +0300

    fix hard mission

--- script.js
+++ script.js
@@ -6,50 +6,60 @@
 
 // —  если пользовательское число равно загаданному, то игра заканчивается и выводит сообщение  "Поздравляю, Вы угадали!!!".
 
-let isNumber = function(n){
+let isNumber = function (n) {
     return !isNaN(parseFloat(n)) && isFinite(n)
 }
 
-function getNumber (num){
+function getNumber(num) {
+    // создаем счетчик
     let count = 10
-    return startGame = (numFromUser) => {
-        if (!isNumber(numFromUser) ){
-            numFromUser = prompt('Введи число!')
-            numFromUser ? startGame(numFromUser) 
-                : alert('Игра окончена')
-        } else if (numFromUser > num ){
+    return startGame = () => {
+        // задаем вопрос
+        numFromUser = prompt('Введи число!')
+        if (!numFromUser) {
+            alert('Игра окончена')
+        } else if (!isNumber(numFromUser)) {
+            startGame()
+        } else if (numFromUser > num) {
             count--
-            alert('Загаданное число меньше, у вас осталось ' + count + ' попыток')
-            if (count === 0) {
-                alert('Попытки закончились')
-            } else {
-                numFromUser = prompt('Введи число!')
-                numFromUser ? startGame(numFromUser) : alert('Игра окончена')
-            }
-        } else if (numFromUser < num){
-            count--
-            alert('Загаданное число меньше, у вас осталось ' + count + ' попыток')
             if (count === 0) {
                 let again = confirm('Попытки закончились, хотите попробовать еще раз?')
-                    if (again){
-                        count = 10
-                        startGame(prompt('Угадай число от 1 до 100'))
-                    }{
-                        return
-                    }
-                     
-            } else {
-                numFromUser = prompt('Введи число!')
-                numFromUser ? startGame(numFromUser) : alert('Игра окончена')
+                if (again) {
+                    count = 10
+                    startGame()
+            } {
+                return
             }
-        } else if (numFromUser = num){
-            alert('Поздравляю, Вы угадали!!!')
+
+        } else {
+            alert('Загаданное число меньше у вас осталось ' + count + ' попыток')
+            startGame()
         }
+
+
+    } else if (numFromUser < num) {
+        count--
+        if (count === 0) {
+            let again = confirm('Попытки закончились, хотите попробовать еще раз?')
+            if (again) {
+                count = 10
+                startGame()
+            } {
+                return
+            }
+
+        } else {
+            alert('Загаданное число больше у вас осталось ' + count + ' попыток')
+            startGame()
+        }
+
+    } else if (numFromUser == num) {
+        alert('Поздравляю, Вы угадали!!!')
     }
-    
+}
+
 }
 
 const startGame1 = getNumber(50)
 
-startGame(prompt('Угадай число от 1 до 100'))
-
+startGame()
